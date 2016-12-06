274e8cf69674bba07d67ea19412c19ac346004e2
Danielle Greenberg <danielle.9897@gmail.com>
2016-12-06 12:11:45 +0200

    Added a remove confirm pop-up

--- SampleApp/src/SampleApp/wwwroot/js/site.js
+++ SampleApp/src/SampleApp/wwwroot/js/site.js
@@ -1,1 +1,6 @@
-﻿// Write your Javascript code.
+﻿
+$(".removeIcon").on('click', (function (event) {
+    if (confirm("Are you sure you want to remove this item ?") == false) {
+        event.preventDefault();
+    }
+}));
