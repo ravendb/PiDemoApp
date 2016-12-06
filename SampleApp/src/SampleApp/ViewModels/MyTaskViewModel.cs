fb7847a9e6cd6d22c912652a62c7a2e6ecfb5337
Danielle Greenberg <danielle.9897@gmail.com>
2016-12-06 11:47:23 +0200

    Add the new view model class

---
+++ SampleApp/src/SampleApp/ViewModels/MyTaskViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleApp.ViewModels
+{
+    public class MyTaskViewModel
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public string TaskToDo { get; set; }
+    }
+}
